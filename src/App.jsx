11454698980db9e112a53b7d5dd60dedf3be179b
AyToasty <danhcao65@gmail.com>
2023-11-03 14:19:13 -0500

    Completed Puppy Pals and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
-import {puppyList} from './data.js'
-import {useState} from 'react'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppyList);
+  const [puppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
   return (
     <>
       <div className='App'>
         {
           puppies.map((puppy) => {
-            return <p key ={puppy.id}>{puppy.name}</p>
+            return <p className='click' onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
           })
         }
+        {featPupId && (
+          <div className='featured'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
